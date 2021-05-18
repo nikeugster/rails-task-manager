8554c54e9e179c4e840ce391a1e01a3a3a817915
Nik Eugster <mail@nikeugster.ch>
2021-05-18 11:29:19 +0200

    fixed wrong colum name

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_092034) do
+ActiveRecord::Schema.define(version: 2021_05_18_092519) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "deatils"
+    t.text "details"
     t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
