198def293ffb3925e3391a91992b1a0644d7b6c4
Nik Eugster <mail@nikeugster.ch>
2021-05-18 15:06:54 +0200

    added default to completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
---
+++ db/migrate/20210518130414_add_default_false_to_tasks_completed.rb
@@ -0,0 +1,5 @@
+class AddDefaultFalseToTasksCompleted < ActiveRecord::Migration[6.0]
+  def change
+    change_column :tasks, :completed, :boolean, default: false
+  end
+end
